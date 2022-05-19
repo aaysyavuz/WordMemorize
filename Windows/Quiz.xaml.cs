771537e85ab2fd5dcc9f4117e49ef19f8aa95c79
aysyavuz <a.yavuz@digitastic.app>
2022-05-19 12:18:56 +0200

    first commit

---
+++ Windows/Quiz.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WordMemorize.Windows
+{
+    /// <summary>
+    /// Interaction logic for Quiz.xaml
+    /// </summary>
+    public partial class Quiz : Window
+    {
+        public Quiz()
+        {
+            InitializeComponent();
+        }
+    }
+}
